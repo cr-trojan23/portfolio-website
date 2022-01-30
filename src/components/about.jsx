e943b5fdcb230d6f05e5b63a9058810ec5c58450
cr-trojan23 <pandusrinu1604@gmail.com>
2022-01-30 19:37:06 +0530

    fix: about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -39,13 +39,14 @@ const AboutSection = () => {
 			<main>
 				<p className="text" id="about-content">
 					I'm glad destiny brought you to my page. I'm Srinivas, a second year
-					undergraduate student at Vellore institute of technology. I am from
-					Hydreabad and I'm obsessed with Amazon Web Services (AWS). I like
-					finance, marketing, and I am a full time Linux User. I started my
-					journey with at the age of 15, and it has been a wonderful experience
-					since then. Welcome, keep exploring, maybe you'll find something
-					interesting and if you have any questions don't hesitate to reach out.
-					Hope this page serves you well.
+					undergraduate student at Vellore institute of technology. I'm obsessed
+					with Amazon Web Services (AWS) and not using windows. I like finance,
+					marketing, and I am a full time Linux User. I fell in love with
+					programming, technology and computers at the age of 15, and it has
+					been a wonderful experience since then.
+					Welcome, keep exploring, maybe
+					you'll find something interesting and if you have any questions don't
+					hesitate to reach out. Hope this page serves you well.
 				</p>
 			</main>
 		</section>
